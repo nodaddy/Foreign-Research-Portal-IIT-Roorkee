bcec54fee4bec8e019d115ff4d409d0a972de3ef
NEELESH SHARMA <nsharma1@me.iitr.ac.in>
2020-02-25 20:50:53 +0530

    commi

--- src/App.js
+++ src/App.js
@@ -30,10 +30,10 @@ function App() {
           <Route exact path="/">
             <Profile personName="Neelesh Sharma" department="Mechanical Department" enrollmentNo="16119014" email="nsharma1@me.iitr.ac.in" PP={logo}></Profile>
           </Route>
-          <Route path="http://nodaddy.github.io/Foreign-Research-Portal/projects">
+          <Route path="https://nodaddy.github.io/Foreign-Research-Portal/projects">
             <Projects/>
           </Route>
-          <Route path="http://nodaddy.github.io/Foreign-Research-Portal/yourapplications">
+          <Route path="https://nodaddy.github.io/Foreign-Research-Portal/yourapplications">
             <ProjectApplications/>
           </Route>
           <Route path="/bookmarks">
