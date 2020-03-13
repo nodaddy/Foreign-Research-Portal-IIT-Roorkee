d60a7f1756a645d44750869ea668261c65ad63fc
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2020-03-13 11:26:07 +0530

    Route Final

--- src/App.js
+++ src/App.js
@@ -11,73 +11,74 @@ import Results from './componenets/Results';
 import Team from './componenets/Team';
 import {AuthRoute} from 'react-router-auth';
 import Profile from './componenets/Profile'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter, Switch} from 'react-router-dom';
 
 const isLoggedIn = !(localStorage.getItem('token') == null || localStorage.getItem('token') === "");
 
 function App() {
     return (
-        <div className="container-fluid App">
-            <TopNav loggedIn={isLoggedIn} />
-            <div className="row" align="center" style={{width: '100%'}}>
-                <div className="col-sm-2" style={{
-                    borderbottom: '0px inset silver',
-                    padding: '0px',
-                    margin: '0px',
-                    height: '87vh',
-                    boxShadow: '2px 2px 7px 0px silver'
-                }}>
-                    <SideNav/>
-                </div>
-                <div className="col-sm-10" style={{margin: '0px', padding: '0px'}} align="left">
-                    <Router>
-                        <Switch>
-                            <AuthRoute
-                                authenticated={!isLoggedIn}
-                                redirectTo='/dashboard'
-                                path='/auth'
-                                component={Auth}/>
-                            <AuthRoute
-                                authenticated={isLoggedIn}
-                                redirectTo='/auth'
-                                path='/dashboard'
-                                component={Profile}/>
-                            <AuthRoute
-                                authenticated={isLoggedIn}
-                                redirectTo='/auth'
-                                path='/projects'
-                                component={Projects}/>
-                            <AuthRoute
-                                authenticated={isLoggedIn}
-                                redirectTo='/auth'
-                                path='/yourapplications'
-                                component={ProjectApplications}/>
-                            <AuthRoute
-                                authenticated={isLoggedIn}
-                                redirectTo='/auth'
-                                path='/bookmarks'
-                                component={Bookmarks}/>
-                            <AuthRoute
-                                authenticated={isLoggedIn}
-                                redirectTo='/auth'
-                                path='/experiences'
-                                component={Experiences}/>
-                            <AuthRoute
-                                authenticated={isLoggedIn}
-                                redirectTo='/auth'
-                                path='/results'
-                                component={Results}/>
-                            <AuthRoute
-                                authenticated={isLoggedIn}
-                                redirectTo='/auth'
-                                path='/team'
-                                component={Team}/>
-                        </Switch>
-                    </Router>
+        <HashRouter>
+            <div className="container-fluid App">
+                <TopNav loggedIn={isLoggedIn}/>
+                <div className="row" align="center" style={{width: '100%'}}>
+                    <div className="col-sm-2" style={{
+                        borderbottom: '0px inset silver',
+                        padding: '0px',
+                        margin: '0px',
+                        height: '87vh',
+                        boxShadow: '2px 2px 7px 0px silver'
+                    }}>
+                        <SideNav/>
+                    </div>
+                    <div className="col-sm-10" style={{margin: '0px', padding: '0px'}} align="left">
+                        <HashRouter>
+                            <Switch>
+                                <AuthRoute
+                                    authenticated={!isLoggedIn}
+                                    redirectTo='/dashboard'
+                                    path='/auth'
+                                    component={Auth}/>
+                                <AuthRoute
+                                    authenticated={isLoggedIn}
+                                    redirectTo='/auth'
+                                    path='/dashboard'
+                                    component={Profile}/>
+                                <AuthRoute
+                                    authenticated={isLoggedIn}
+                                    redirectTo='/auth'
+                                    path='/projects'
+                                    component={Projects}/>
+                                <AuthRoute
+                                    authenticated={isLoggedIn}
+                                    redirectTo='/auth'
+                                    path='/yourapplications'
+                                    component={ProjectApplications}/>
+                                <AuthRoute
+                                    authenticated={isLoggedIn}
+                                    redirectTo='/auth'
+                                    path='/bookmarks'
+                                    component={Bookmarks}/>
+                                <AuthRoute
+                                    authenticated={isLoggedIn}
+                                    redirectTo='/auth'
+                                    path='/experiences'
+                                    component={Experiences}/>
+                                <AuthRoute
+                                    authenticated={isLoggedIn}
+                                    redirectTo='/auth'
+                                    path='/results'
+                                    component={Results}/>
+                                <AuthRoute
+                                    authenticated={isLoggedIn}
+                                    redirectTo='/auth'
+                                    path='/team'
+                                    component={Team}/>
+                            </Switch>
+                        </HashRouter>
+                    </div>
                 </div>
             </div>
-        </div>
-
+        </HashRouter>
     );
 }
 
--- src/componenets/SideNav.js
+++ src/componenets/SideNav.js
@@ -1,36 +1,36 @@
 import React, {Component} from 'react';
 import './SideNav.css'
-import {BrowserRouter as Router,Route,Switch,Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-class SideNav extends Component{
-    render(){
-        return(
+class SideNav extends Component {
+    render() {
+        return (
             <div className="container-fluid"><br/>
-               <div className="row sideNavLink" align="center">
-                 <a href="/projects" id="projects" className="col-sm-12">Projects</a>
-                 </div>
                 <div className="row sideNavLink" align="center">
-                    <a href="/yourapplications"  id="yourApplications" className="col-sm-12">Your Applications</a>
-                 </div>
-                 <div className="row sideNavLink" align="center">
-                    <a href="/bookmarks" id="bookmarks" className="col-sm-12">Bookmarks</a>
-                    </div>
-                    <div className="row sideNavLink" align="center">
-                    <a href="/results" id="results" className="col-sm-12">Results</a>
-                    </div>
-                    <hr/>
-                    <div className="row sideNavLink" align="center">
-                    <a href="/experiences"  id="experience" className="col-sm-12 sideNavLink1">Experience</a>
-                    </div>
-                    <hr/>
-                    <div className="row sideNavLink" align="center">
-                    <a href="/team" id="team" className="col-sm-12 sideNavLink1">Team</a>
-                    </div>
-                   
+                    <Link to="/projects" id="projects" className="col-sm-12">Projects</Link>
                 </div>
-         
+                <div className="row sideNavLink" align="center">
+                    <Link to="/yourapplications" id="yourApplications" className="col-sm-12">Your Applications</Link>
+                </div>
+                <div className="row sideNavLink" align="center">
+                    <Link to="/bookmarks" id="bookmarks" className="col-sm-12">Bookmarks</Link>
+                </div>
+                <div className="row sideNavLink" align="center">
+                    <Link to="/results" id="results" className="col-sm-12">Results</Link>
+                </div>
+                <hr/>
+                <div className="row sideNavLink" align="center">
+                    <Link to="/experiences" id="experience" className="col-sm-12 sideNavLink1">Experience</Link>
+                </div>
+                <hr/>
+                <div className="row sideNavLink" align="center">
+                    <Link to="/team" id="team" className="col-sm-12 sideNavLink1">Team</Link>
+                </div>
+
+            </div>
+
         );
     }
-} 
+}
 
 export default SideNav;
