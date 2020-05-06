c078048cefc99cd439a101445368ffd0ee89f2ad
NEELESH SHARMA <nsharma1@me.iitr.ac.in>
2020-05-07 00:51:58 +0530

    jk

--- src/componenets/Applications.js
+++ src/componenets/Applications.js
@@ -1,7 +1,8 @@
 import React,{Component} from 'react'
 import './Applications.css'
 import {withRouter} from 'react-router-dom';
-
+import axios from "axios"
+import {API_ENDPOINT} from "../config";
 
 
 function ApplicationDetails(props){
@@ -26,8 +27,58 @@ function ApplicationDetails(props){
 }
 
 
+
 class Applications extends Component{
 //Component to be rendered through App.js
+    state={
+        appliedProjectsArray: [],
+        acceptedProjects:[],
+    }
+
+    componentDidMount(){
+        axios
+            .get(API_ENDPOINT + 'users/myapps/',
+            {
+                headers: {
+                    Authorization: 'Token ' + localStorage.getItem('token')
+                }
+            }
+            ).then((res)=>{
+                
+                let strr = res.data.split('~');
+                this.setState({appliedProjectsArray: strr}); 
+
+            }) 
+//write .catch()
+            axios
+            .get(API_ENDPOINT + 'users/myaccepted/',
+            {
+                headers: {
+                    Authorization: 'Token ' + localStorage.getItem('token')
+                }
+            }
+            ).then((res)=>{
+                
+                let strr = res.data.split('~');
+                this.setState({acceptedProjects: strr}); 
+
+            })
+//write .catch()
+           /* var i;
+            var k=[];
+            for(i=0;i<this.state.acceptedProjects.length;i++){
+                if(this.state.appliedProjectsArray.includes(this.state.acceptedProjects[i])){
+                    var tit = this.state.acceptedProjects[i];
+                    alert("f");
+                    var index = this.state.appliedProjectsArray.indexOf(tit)
+                    k = this.state.appliedProjectsArray;
+                    k.splice(index, 1);
+                    this.setState({"appliedProjectsArray":k});
+                }
+            }*/
+            
+    }
+
 
     render(){
         return(
@@ -40,15 +91,20 @@ class Applications extends Component{
                   
                     <div className="col-sm-12" id="ProjectDetailsArea">
                     {/*ProjectDetail is function Component defined in the above code*/}
-                    <ApplicationDetails title="Enhance Photoelectrochemistry by Titanium Nanotube to Realize Early Detection of Alzheimer's Disease
-" university="Chang Gung University
-" deadline=" 3rd Feb 2020
-" statusColor="#53E010"></ApplicationDetails>
-                   <ApplicationDetails title="Enhance Photoelectrochemistry by Titanium Nanotube to Realize Early Detection of Alzheimer's Disease
-" university="Chang Gung University
-" deadline="21st Feb 2020
-" statusColor="#53E010"></ApplicationDetails>
-
+                    {
+                        this.state.acceptedProjects.map(function(obj,index){
+                        if(obj!=" " && obj!=""){
+                        var anArray = obj.split('*');
+                        var pTitle = anArray[0];
+                        var pUniversity = anArray[1];
+                        var pDeadline = anArray[2];
+                        return(
+                            <ApplicationDetails title={pTitle} university={pUniversity} deadline={pDeadline} statusColor="#93FF33"></ApplicationDetails>
+                        ) 
+                        }
+                    })
+                    } 
+                  
                     </div>
                 </div>
                 <br/><hr/>
@@ -57,24 +113,42 @@ class Applications extends Component{
                 <div className="row" align="center">
                     <div className="col-sm-12" id="PastProjectDetailsArea">
                     {/*ProjectDetail is function Component defined in the above code*/}
-                    <ApplicationDetails title="Project Title :" university="University :" deadline="Deadline :" statusColor="#F8D044"></ApplicationDetails>
-                                 
+                    
+                    {   
+                        
+                       // var acceptedProjArray = this.state.acceptedProjects;
+                        this.state.appliedProjectsArray.map((obj,index)=>{
+                        if(obj!=" " && obj!=""){
+                        var anArray = obj.split('*');
+                        var pTitle = anArray[0];
+                        var pUniversity = anArray[1];
+                        var pDeadline = anArray[2];
+                        
+                        return(
+                            <ApplicationDetails className={pTitle} title={pTitle} university={pUniversity} deadline={pDeadline}></ApplicationDetails>
+                        ) 
+                        
+                    }
+                    })
+                    }
+                                        
+                               
 
                     </div>
                 </div>
 
                 <br/><hr/>
-                <h4>Rejected</h4>
+                {/*<h4>Rejected</h4>
                 <br/>
                 <div className="row" align="center">
                     <div className="col-sm-12" id="PastProjectDetailsArea">
-                    {/*ProjectDetail is function Component defined in the above code*/}
+                    {/*ProjectDetail is function Component defined in the above code}
                     <ApplicationDetails title="Project Title :" university="University :" deadline="Deadline :" statusColor="#FC7A7A"></ApplicationDetails>
                                  
              
 
                     </div>
-                </div>
+                </div>*/}
             </div>
         );
     }
--- src/componenets/Projects.js
+++ src/componenets/Projects.js
@@ -9,6 +9,7 @@ import {API_ENDPOINT} from "../config";
 
 function ProjectDetails(props){
 //Component to show project details
+    var vl="";
     var i = 1;
     return(
         <div className="projectDetail" align="left" style={{width:'100%'}}>
@@ -26,18 +27,38 @@ function ProjectDetails(props){
                 <div className="row projectDes" id={props.title}>
                     <div className="col-sm-12" align="left">
                     <h4 align="center" style={{color:'#0B275B', fontFamily:'roboto'}}><b>Project Details</b></h4><br/>
-                    <div className="row"><h5><b>Description</b></h5>{props.title}</div><br/>
+                    <div className="row"><h5><b>Description</b></h5>{props.description}</div><br/>
                     <div className="row"><h5><b>Key Tasks</b></h5>{props.keyTasks}</div><br/>
                     <div className="row"><h5><b>Critical Skills</b></h5>{props.criticalSkills}</div><br/>
                     <div className="row"><h5><b>Stipend</b></h5>{props.stipend}</div><br/>
                     <div className="row"><h5><b>Duration</b></h5>{props.duration}</div><br/>
                     <div className="row"><h5><b>Eligibility</b></h5>{props.eligibility}</div><br/>
+                    <div className="row"><h5><b>Contact Details</b></h5>{props.contactDetails}</div><br/>
                     <div className="row" align="center"><h5><b>A statement of the importance of the proposed research to your future (250 words)
 </b></h5>
-                    <textarea id="proposal" rows="10" cols="90"></textarea>
+                    
+                    <textarea onChange={(e)=>{
+                        vl = e.target.value;
+                    }} id="proposal" rows="10" cols="90"></textarea>
+                    
                     </div>
                     <div className="row" align="center">
-                    <button align="center" style={{backgroundColor:'white',color:'green'}}><b>Submit</b></button>
+                    <button onClick={()=>{
+                       
+                       //alert(vl);
+                       axios.post(
+                           API_ENDPOINT + 'users/apply/',
+                           {
+                                "proposal":vl,
+                                "pTitle":props.title,  
+                           },
+                           {
+                               headers: {
+                                   Authorization: 'Token ' + localStorage.getItem('token')
+                               }
+                           }
+                       )
+                    }} align="center" style={{backgroundColor:'white',color:'green'}}><b>Submit</b></button>
                     </div>
                     </div>
                 </div>
@@ -65,6 +86,7 @@ class Projects extends Component{
             })
         .then((res) => {
             let projs = JSON.parse(res.data.jsonStuff);
+           // alert(`something went wrong`)
             this.setState({projectss:projs});
             //console.log("dfghjkl");
             //console.log(this.state.projectss[0].fields.title);
@@ -77,26 +99,7 @@ class Projects extends Component{
     }
 
 
-    apply = function(){
-       document.getElementsByClassName('projectDes').style.display = 'block';
-        /*axios
-            .get(
-                API_ENDPOINT + 'users/apply',{
-                    title_of_project: "fdhf",
-                },
-                {
-                    headers: {
-                        Authorization: 'Token ' + localStorage.getItem('token')
-                    }
-                })
-            .then((res) => {
-                //alert(res.data.hg)
-                //window.location.reload();
-            })
-            .catch(() => {
-                alert(``)
-            })*/
-    }.bind(this)
+    
 
     render(){
         return(
@@ -108,11 +111,20 @@ class Projects extends Component{
                         let tit = obj.fields.title;
                         let uni = obj.fields.university;
                         let deadl = obj.fields.deadline;
+                        let des = obj.fields.description;
+                        let keyTasks = obj.fields.key_tasks;
+                        let criticalSkills = obj.fields.critical_skills;
+                        let stipend = obj.fields.stipend;
+                        let duration = obj.fields.duration_in_months;
+                        let eligibility = obj.fields.eligibility;
+                        let contactDetails = obj.fields.contact_email;
+
+                        console.log(obj);
                         return(
                             <div className="row" align="center" style={{padding:'0px',width:'100%'}}>
                             <div className="col-sm-12" id="ProjectDetailsArea">
                             {/*ProjectDetail is function Component defined in the above code*/}
-                            <ProjectDetails className="pdetail" title={tit} university={uni} deadline={deadl}></ProjectDetails><br/>
+                            <ProjectDetails className="pdetail" contactDetails={contactDetails} description={des} keyTasks={keyTasks} criticalSkills={criticalSkills} eligibility={eligibility} stipend={stipend} duration={duration} title={tit} university={uni} deadline={deadl}></ProjectDetails><br/>
        
                             
                        </div>
